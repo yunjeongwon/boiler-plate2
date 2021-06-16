c2fca9455ed392eb98d30cf6aecff4ba552cc92c
jeongwon <yunjwon94@gmail.com>
2021-06-16 18:46:26 +0900

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://johnahn:abcd1234@boilerplate.fgv8c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    }
+  )
+  .then(() => console.log("MongDB Connected"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
